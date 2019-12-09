bae5f0e211646f71f1f4d15663403fb6f34ae5f0
Michael DeSantis <desanmichael1@gmail.com>
2019-12-08 19:36:16 -0600

    Updates

---
+++ precache-manifest.f3b6a82624d5af428efd36b24b977770.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355b459965da663fb18917765a3b6ffa",
+    "url": "/dev_portfolio/index.html"
+  },
+  {
+    "revision": "bd052dfa03ebaf9b087c",
+    "url": "/dev_portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "7a63ce74d6cad866e059",
+    "url": "/dev_portfolio/static/css/main.0229031c.chunk.css"
+  },
+  {
+    "revision": "bd052dfa03ebaf9b087c",
+    "url": "/dev_portfolio/static/js/2.04b59f88.chunk.js"
+  },
+  {
+    "revision": "7a63ce74d6cad866e059",
+    "url": "/dev_portfolio/static/js/main.8d59f813.chunk.js"
+  },
+  {
+    "revision": "c5a10809fa212d048dbe",
+    "url": "/dev_portfolio/static/js/runtime-main.0dccc465.js"
+  },
+  {
+    "revision": "48034d5d7056fdfd2422c033d7a63290",
+    "url": "/dev_portfolio/static/media/booklist.48034d5d.jpg"
+  },
+  {
+    "revision": "0d327c70988b3abcc1a589730fc6c6bd",
+    "url": "/dev_portfolio/static/media/jscalc.0d327c70.jpeg"
+  },
+  {
+    "revision": "f66789b21250d4ec301ae055da0e4041",
+    "url": "/dev_portfolio/static/media/nasa.f66789b2.png"
+  },
+  {
+    "revision": "9be22bee81b89f44b3112113e10c360f",
+    "url": "/dev_portfolio/static/media/weather.9be22bee.jpg"
+  }
+]);
